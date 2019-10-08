b6003c963f53b36c662773e1faff9a0574e752e7
Timothée Darcet <timothee.darcet+git@m4x.org>
2019-10-09 01:31:13 +0200

    Format

--- calculate_opti.py
+++ calculate_opti.py
@@ -1,7 +1,8 @@
 maxN = 64
 
 def value(k):
-    return lambda n: n // k + n % k
+    return lambda n: (n // k) + (n % k)
 
-for k in range(maxN + 1):
-    print(k, sum(filter(value(k), range(maxN + 1))) / (maxN + 1))
+for k1 in range(1, 16):
+    for k2 in range(1, k1):
+        print(k1, k2, sum((n // k1) + (n % k1) // k2 + (n % k1) % k2 for n in range(maxN + 1)) / (maxN + 1))
